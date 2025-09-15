e6b1816adb8006de115e4d94f4191d783311c8b4
VladK333 <vladanaaleksic3@gmail.com>
2025-09-15 22:19:02 +0200

    Dodate fje za formiranje zahteva i parsiranje odgovora (Read Coils)

--- dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -1,6 +1,7 @@
 ﻿using Common;
 using Modbus.FunctionParameters;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
@@ -24,15 +25,48 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] rqst = new byte[12];
+            ModbusReadCommandParameters ModbusRead = this.CommandParameters as ModbusReadCommandParameters;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.TransactionId)), 0, rqst, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.ProtocolId)), 0, rqst, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.Length)), 0, rqst, 4, 2);
+            rqst[6] = ModbusRead.UnitId;
+            rqst[7] = ModbusRead.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.StartAddress)), 0, rqst, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModbusRead.Quantity)), 0, rqst, 10, 2);
+
+            return rqst;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters ModbusRead = this.CommandParameters as ModbusReadCommandParameters;
+
+            Dictionary<Tuple<PointType, ushort>, ushort> dictionary = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int cnt = 0;
+            ushort adresa = ModbusRead.StartAddress;
+            ushort value;
+            byte maska = 1;
+            for (int i = 0; i < response[8]; i++)
+            {
+                byte tempbyte = response[9 + i];
+                for (int j = 0; j < 8; j++)
+                {
+                    value = (ushort)(tempbyte & maska);
+                    tempbyte >>= 1;
+                    dictionary.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, adresa), value);
+                    cnt++;
+                    adresa++;
+                    if (cnt == ModbusRead.Quantity)
+                    {
+                        break;
+                    }
+
+                }
+            }
+            return dictionary;
         }
     }
 }
